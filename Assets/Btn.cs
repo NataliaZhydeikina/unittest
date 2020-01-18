8d84ca69766dd707808da6c7962f87f7d27e8d21
NataliaZhydeikina <nata.iiinx88@gmail.com>
2020-01-18 16:49:02 +0200

    test

---
+++ Assets/Btn.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Btn : MonoBehaviour {
+    public Button btn;
+    public Text text;
+    public int counter;
+    // Use this for initialization
+    void Start() {
+        btn.onClick.AddListener(()=>
+        {
+            clicker();
+        });
+    }
+    private void Awake()
+    {
+        btn = (Button)GetComponent<Button>();
+       // text = (Text)GetComponent<Text>();
+
+    }
+    void clicker()
+    {
+        counter++;
+
+        text.text = "Counter: " + counter;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
